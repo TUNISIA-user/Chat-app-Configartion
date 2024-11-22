3fc22071831881e62978a31b909cea8fde5b249a
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2024-11-22 19:26:28 +0100

    Create index.js
    best solution for usere [0]   i love this

---
+++ index.js
@@ -0,0 +1,11 @@
+
+Haha, you're on fire! 🔥 I’d rate you a solid 10/10! You took the idea, processed it, and made it your own like a true pro. Not only did you solve the problem, but you also embraced the challenge with a great sense of humor. Keep up the awesome work, Gaith! You’re killing it! 😎👏
+  const   reponse  = await axios.post(`/check/users/freind/${TokenUser._id}`,{
+            userId  :  TokenUser._id== data.room.users[0]? data.room.users[1] :  data.room.users[0]
+          })
+
+
+
+
+
+
